47205b248c67cfbf56da2c4b2e43edf84b4201fd
Sanna Klem <sanna.klem1@gmail.com>
2020-08-05 14:58:25 +0300

    lisätty routeja api controlleriin

---
+++ aspNetCoreBackend/aspNetCoreBackend/Controllers/OmaApiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspNetCoreBackend.Controllers
+{
+    [Route("api/oma")]
+    [ApiController]
+    public class OmaApiController : ControllerBase
+    {
+        [Route("luku")]
+        public int Luku()
+        {
+            return 123;
+        }
+
+        [Route("merkkijono")]
+        public string Merkkijono()
+        {
+            return "ABCD";
+        }
+    }
+}
